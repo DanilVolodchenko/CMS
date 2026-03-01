a6c39cea2aabb6ac600501d3bea91eb0ced26099
Danil Volodchenko <danil.volodchenko@inbox.ru>
2026-03-01 12:16:22 +0700

    modifing base component

--- components/base.py
+++ components/base.py
@@ -1,31 +1,16 @@
-import dataclasses
 from typing import Any
-from annotationlib import ForwardRef
 
-from schemas.base import BaseShema
+from dispatcher import FieldDispatcher
 
 
 class BaseComponent:
     name: str
-    schema: BaseShema
+    schema: Any
 
     @classmethod
     def generate_schema(cls) -> dict[str, Any]:
         """Generate schema."""
 
-        instance = cls()
+        dispatcher = FieldDispatcher()
 
-        return {'name': cls.name, 'fields': instance._get_fields(cls.schema)}
-
-    def _get_fields(self, obj: BaseShema) -> dict[str, Any]:
-        fields = {}
-
-        for field in dataclasses.fields(obj):
-            if dataclasses.is_dataclass(field.type):
-                fields[field.name] = self._get_fields(field.type)  # noqa
-            elif isinstance(field.type, ForwardRef):
-                fields[field.name] = self._get_fields(field.type.evaluate())
-            else:
-                fields[field.name] = field.type.__name__
-
-        return fields
+        return {'name': cls.name, 'fields': dispatcher.generate(cls.schema)}
