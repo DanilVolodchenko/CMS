1b612a60f2817e037490b9ee7d62c094b8955022
Danil Volodchenko <danil.volodchenko@inbox.ru>
2026-03-07 14:44:32 +0700

    creating domain entity

--- domain/storage.py
+++ domain/storage.py
@@ -1,8 +1,14 @@
+from typing import Any
+
+
 class ComponentStorage:
 
     def __init__(self) -> None:
-        self.components = {}
+        self._components = {}
 
 
-    def add_components(self):
-        ...
+    def get_components(self, page: str) -> list[dict[str, Any]]:
+        return self._components[page]
+
+    def add_components(self, page: str, components: list[dict[str, Any]]) -> None:
+        self._components[page] = components
