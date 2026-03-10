c17075e908eac4406f8664037da8425e959a19ae
danil <da.volodchenko@sm117.ru>
2026-03-10 10:16:26 +0700

    fixing tests

--- tests/unit/infrastructure/resources/test_storage.py
+++ tests/unit/infrastructure/resources/test_storage.py
@@ -15,14 +15,14 @@ class TestComponentStorage:
         """Testing method get_component if it is empty."""
 
         with pytest.raises(KeyError):
-            component_storage.get_components('/test')
+            component_storage.get_components_by_page('/test')
 
     def test_get_component_if_storage_not_empty(self, component_storage: ComponentStorage) -> None:
         """Testing method get_component if it is not empty."""
 
         component_storage._components['/test'] = [{'component': '1'}]
 
-        result = component_storage.get_components('/test')
+        result = component_storage.get_components_by_page('/test')
         expected_result = [{'component': '1'}]
 
         assert result == expected_result, f'Expected result {expected_result}, result {result}'
