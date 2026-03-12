36eaed5217e09fe46ecd2df6badc5a879455146b
Danil Volodchenko <danil.volodchenko@inbox.ru>
2026-03-12 13:15:56 +0700

    adding tests for generators

---
+++ tests/unit/infrastructure/generators/test_dataclass.py
@@ -0,0 +1,74 @@
+import dataclasses
+from typing import Any
+
+import pytest
+
+from infrastructure.generators.dataclass import DataclassGenerator
+from infrastructure.dispatcher import IFieldDispatcher
+
+
+@pytest.fixture
+def dataclass_generator() -> DataclassGenerator:
+    return DataclassGenerator()
+
+
+@pytest.fixture
+def dispatcher() -> IFieldDispatcher:
+    class MockDispatcher(IFieldDispatcher):
+        def generate(self, schema: Any) -> Any:
+            return str
+
+        def get_type_name(self, obj: Any) -> str: ...
+
+    return MockDispatcher()
+
+
+@dataclasses.dataclass
+class MockDataclassSchema:
+    field: str
+
+
+class TestDataclassGenerator:
+    """Tests suite for DataclassGenerator."""
+
+    def test_supports_if_schema_is_dataclass(self, dataclass_generator: DataclassGenerator) -> None:
+        """Tests supports method if schema is dataclass."""
+
+        schema = MockDataclassSchema
+
+        result = dataclass_generator.supports(schema)
+        expected_result = True
+
+        assert result == expected_result, f'Expected result {expected_result}, result {result}'
+
+    def test_supports_if_schema_is_not_dataclass(self, dataclass_generator: DataclassGenerator) -> None:
+        """Tests supports method if schema is not dataclass."""
+
+        schema = []
+
+        result = dataclass_generator.supports(schema)
+        expected_result = False
+
+        assert result == expected_result, f'Expected result {expected_result}, result {result}'
+
+    def test_generate_if_schema_is_dataclass(
+            self, dataclass_generator: DataclassGenerator, dispatcher: IFieldDispatcher
+    ) -> None:
+        """Tests generate method if schema is dataclass."""
+
+        schema = MockDataclassSchema
+
+        result = dataclass_generator.generate(schema, dispatcher)
+        expected_result = {'field': str}
+
+        assert result == expected_result, f'Expected result {expected_result}, result {result}'
+
+    def test_generate_if_schema_is_not_dataclass(
+            self, dataclass_generator: DataclassGenerator, dispatcher: IFieldDispatcher
+    ) -> None:
+        """Tests generate method if schema is not dataclass."""
+
+        schema = []
+
+        with pytest.raises(TypeError):
+            dataclass_generator.generate(schema, dispatcher)
